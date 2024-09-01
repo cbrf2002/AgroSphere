2f0b9b3d71657182d820aed2499de14c5f84a598
cbrf2002 <charlesbryan.fabian10@gmail.com>
2024-09-01 13:12:51 +0800

    v0.2.2

---
+++ app/src/main/java/com/fsvdevs/agrosphere/DashboardScreen.kt
@@ -0,0 +1,67 @@
+package com.fsvdevs.agrosphere
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.navigation.compose.NavHost
+import com.fsvdevs.agrosphere.ui.theme.AgroSphereTheme
+import com.fsvdevs.agrosphere.ui.theme.AppTypography
+
+@Composable
+fun dashboardScreen(navController: NavController) {
+    AgroSphereTheme(isSystemInDarkTheme()) {
+        // State to manage the selected item in the navigation bar
+        var selectedItem by remember { mutableStateOf(0) }
+
+        Scaffold(
+            modifier = Modifier
+                .fillMaxSize()
+                .systemBarsPadding(),
+            containerColor = MaterialTheme.colorScheme.surfaceContainerLow
+        ) { paddingValues ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Dashboard!",
+                    style = AppTypography.labelSmall,
+                    textAlign = TextAlign.Center,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun dashboardScreenPreview() {
+    dashboardScreen(navController = androidx.navigation.compose.rememberNavController())
+}
