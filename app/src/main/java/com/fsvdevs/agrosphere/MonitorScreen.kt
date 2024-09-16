be170f4d6001ed87befd812252fe701d9b5104ec
cbrf2002 <charlesbryan.fabian10@gmail.com>
2024-09-16 12:55:19 +0800

    v0.0.4

--- app/src/main/java/com/fsvdevs/agrosphere/MonitorScreen.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/MonitorScreen.kt
@@ -5,14 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.ThumbUp
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,16 +17,13 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.fsvdevs.agrosphere.ui.theme.AgroSphereTheme
 import com.fsvdevs.agrosphere.ui.theme.AppTypography
 
 @Composable
-fun monitorScreen(navController: NavController) {
+fun MonitorScreen(navController: NavController) {
     AgroSphereTheme(isSystemInDarkTheme()) {
         // State to manage the selected item in the navigation bar
         var selectedItem by remember { mutableStateOf(0) }
@@ -63,6 +53,6 @@ fun monitorScreen(navController: NavController) {
 
 @Preview
 @Composable
-fun monitorScreenPreview() {
-    monitorScreen(navController = rememberNavController())
+fun MonitorScreenPreview() {
+    MonitorScreen(navController = rememberNavController())
 }
