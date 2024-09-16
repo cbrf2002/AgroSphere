017f2c981724ad911b58ce0fa5ba09f5e7465a18
cbrf2002 <charlesbryan.fabian10@gmail.com>
2024-09-16 22:25:26 +0800

    Added retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,9 @@ dependencies {
     implementation(libs.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
