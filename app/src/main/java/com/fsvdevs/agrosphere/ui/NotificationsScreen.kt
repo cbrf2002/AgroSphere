d7e3fc778ac8fbaf0e4bb071e5f6ce2222b12661
cbrf2002 <cbrf2002@proton.me>
2024-10-01 00:40:08 +0800

    Modified screen.

--- app/src/main/java/com/fsvdevs/agrosphere/ui/NotificationsScreen.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/ui/NotificationsScreen.kt
@@ -1,7 +1,9 @@
-package com.fsvdevs.agrosphere
+package com.fsvdevs.agrosphere.ui
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
@@ -14,45 +16,38 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.fsvdevs.agrosphere.ui.theme.AgroSphereTheme
 import com.fsvdevs.agrosphere.ui.theme.AppTypography
 
 @Composable
-fun MonitorScreen(navController: NavController) {
+fun NotificationsScreen(navController: NavController) {
     AgroSphereTheme(isSystemInDarkTheme()) {
         // State to manage the selected item in the navigation bar
         var selectedItem by remember { mutableStateOf(0) }
 
-        Scaffold(
+        Column (
             modifier = Modifier
                 .fillMaxSize()
                 .systemBarsPadding(),
-            containerColor = MaterialTheme.colorScheme.surfaceContainerLow
-        ) { paddingValues ->
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(paddingValues),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = "monitor!",
-                    style = AppTypography.labelSmall,
-                    textAlign = TextAlign.Center,
-                    color = MaterialTheme.colorScheme.onBackground
-                )
-            }
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Notifications!",
+                style = AppTypography.labelSmall,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
 
 @Preview
 @Composable
-fun MonitorScreenPreview() {
+fun NotificationsScreenPreview() {
     MonitorScreen(navController = rememberNavController())
 }
