9b08c6e9041dff9cd90cde9230bfd23ae29dd2ef
cbrf2002 <cbrf2002@proton.me>
2024-10-01 00:40:14 +0800

    Modified screen.

--- app/src/main/java/com/fsvdevs/agrosphere/ui/PreferencesScreen.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/ui/PreferencesScreen.kt
@@ -1,8 +1,10 @@
-package com.fsvdevs.agrosphere
+package com.fsvdevs.agrosphere.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
@@ -15,10 +17,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.fsvdevs.agrosphere.ui.theme.AgroSphereTheme
 import com.fsvdevs.agrosphere.ui.theme.AppTypography
@@ -29,26 +31,18 @@ fun PreferencesScreen(navController: NavController) {
         // State to manage the selected item in the navigation bar
         var selectedItem by remember { mutableStateOf(0) }
 
-        Scaffold(
+        Column (
             modifier = Modifier
                 .fillMaxSize()
                 .systemBarsPadding(),
-            containerColor = MaterialTheme.colorScheme.surfaceContainerLow
-        ) { paddingValues ->
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(paddingValues)
-                    .background(MaterialTheme.colorScheme.surfaceContainerLow),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = "preferences!",
-                    style = AppTypography.labelSmall,
-                    textAlign = TextAlign.Center,
-                    color = MaterialTheme.colorScheme.onBackground
-                )
-            }
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Preferences!",
+                style = AppTypography.labelSmall,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
