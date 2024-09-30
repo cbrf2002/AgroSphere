2309ecba7128b3b16bdad1a6c8b2e2d91952e9b9
cbrf2002 <cbrf2002@proton.me>
2024-10-01 00:35:14 +0800

    Added routes

--- app/src/main/java/com/fsvdevs/agrosphere/routes/Routes.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/routes/Routes.kt
@@ -1,7 +1,9 @@
-package com.fsvdevs.agrosphere
+package com.fsvdevs.agrosphere.routes
 
 object Routes {
     const val DASHBOARD_SCREEN = "dashboard"
     const val MONITOR_SCREEN = "monitor"
+    const val NOTIFICATIONS_SCREEN = "notifications"
     const val PREFERENCES_SCREEN = "preferences"
+    const val LOGIN_SCREEN = "login"
 }
