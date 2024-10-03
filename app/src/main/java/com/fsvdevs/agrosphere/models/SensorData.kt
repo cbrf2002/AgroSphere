d76bd7adfa5934502ab3885c466b0cae37621434
cbrf2002 <cbrf2002@proton.me>
2024-10-03 13:59:23 +0800

    Migrated to Realtime Database

--- app/src/main/java/com/fsvdevs/agrosphere/models/SensorData.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/models/SensorData.kt
@@ -1,27 +1,11 @@
 package com.fsvdevs.agrosphere.models
 
-import com.google.firebase.Timestamp
-import com.google.firebase.firestore.PropertyName
-
 data class SensorData(
-    @get:PropertyName("timestamp") @set:PropertyName("timestamp")
-    var timestamp: Timestamp = Timestamp.now(),
-
-    @get:PropertyName("temperature") @set:PropertyName("temperature")
-    var temperature: Float = 0.0f,
-
-    @get:PropertyName("humidity") @set:PropertyName("humidity")
-    var humidity: Float = 0.0f,
-
-    @get:PropertyName("light_level") @set:PropertyName("light_level")
-    var lightLevel: Int = 0,
-
-    @get:PropertyName("water_level") @set:PropertyName("water_level")
-    var waterLevel: Float = 0.0f,
-
-    @get:PropertyName("water_temperature") @set:PropertyName("water_temperature")
-    var waterTemperature: Float = 0.0f,
-
-    @get:PropertyName("ph_level") @set:PropertyName("ph_level")
-    var phLevel: Float = 0.0f
+    var humidity: Double = 0.0,
+    var lightLevel: Double = 0.0,
+    var pH: Double = 0.0,
+    var temperature: Double = 0.0,
+    var timestamp: Long = 0L,
+    var waterLevel: Double = 0.0,
+    var waterTemp: Double = 0.0
 )
