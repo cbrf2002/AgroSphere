112c20e8f7e23ada01d58e79df44ae3c7d809df4
cbrf2002 <cbrf2002@proton.me>
2024-10-03 13:59:06 +0800

    Migrated to Realtime Database

--- app/src/main/java/com/fsvdevs/agrosphere/ui/LoginScreen.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/ui/LoginScreen.kt
@@ -40,9 +40,11 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.fsvdevs.agrosphere.R
 
 @Composable
@@ -235,4 +237,16 @@ fun TextLogo() {
     }
 
     Text(text = text)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen(
+        navController = rememberNavController(),
+        onGoogleSignIn = {},
+        onEmailSignIn = { _, _ -> },
+        onSignUp = { _, _ -> },
+        onForgotPassword = {}
+    )
 }
