ec9f2cc6286018b97f03344e6fbb6e7859fd2a80
cbrf2002 <cbrf2002@proton.me>
2024-10-03 13:57:11 +0800

    Migrated to Realtime Database

--- app/src/main/java/com/fsvdevs/agrosphere/models/ActuatorData.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/models/ActuatorData.kt
@@ -1,18 +1,7 @@
 package com.fsvdevs.agrosphere.models
 
-import com.google.firebase.Timestamp
-import com.google.firebase.firestore.PropertyName
-
 data class ActuatorData(
-    @get:PropertyName("timestamp") @set:PropertyName("timestamp")
-    var timestamp: Timestamp = Timestamp.now(),
-
-    @get:PropertyName("fan_status") @set:PropertyName("fan_status")
-    var fanStatus: Boolean = false,
-
-    @get:PropertyName("fog_status") @set:PropertyName("fog_status")
-    var fogStatus: Boolean = false,
-
-    @get:PropertyName("vent_status") @set:PropertyName("vent_status")
-    var ventStatus: Boolean = false
+    var fan: Boolean = false,
+    var mist: Boolean = false,
+    var vent: Boolean = false
 )
