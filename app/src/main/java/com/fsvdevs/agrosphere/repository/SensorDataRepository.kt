dbcdb99a559913468bfe630d1abcea1826ed5544
cbrf2002 <cbrf2002@proton.me>
2024-10-03 13:59:28 +0800

    Migrated to Realtime Database

--- app/src/main/java/com/fsvdevs/agrosphere/repository/SensorDataRepository.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/repository/SensorDataRepository.kt
@@ -1,30 +1,32 @@
 package com.fsvdevs.agrosphere.repository
 
+
 import android.util.Log
 import com.fsvdevs.agrosphere.models.SensorData
-import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.database.DatabaseReference
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.tasks.await
 
-class SensorDataRepository {
-    private val db = FirebaseFirestore.getInstance()
-    private val sensorCollection = db.collection("sensorData")
+class SensorDataRepository(private val database: DatabaseReference) {
+    private val tag = "SensorDataRepository"
 
-    suspend fun getLatestSensorData(): SensorData? {
-        return try {
-            val querySnapshot = sensorCollection
-                .orderBy("timestamp", com.google.firebase.firestore.Query.Direction.DESCENDING)
-                .limit(1)
-                .get().await()
+    private val _sensorData = MutableStateFlow<SensorData?>(null)
+    val sensorData: StateFlow<SensorData?> = _sensorData
 
-            if (querySnapshot.documents.isNotEmpty()) {
-                querySnapshot.documents.firstOrNull()?.toObject(SensorData::class.java)
+    suspend fun fetchSensorData() {
+        try {
+            val sensorDataSnapshot = database.child("sensorData").get().await()
+            if (sensorDataSnapshot.exists()) {
+                val data = sensorDataSnapshot.getValue(SensorData::class.java)
+                _sensorData.value = data
+                Log.d(tag, "Fetched sensor data: $data")
             } else {
-                Log.e("SensorDataRepository", "No sensor data found")
-                null
+                Log.w(tag, "No sensor data found at 'sensorData'")
+                Log.d(tag, "sensorDataSnapshot: ${sensorDataSnapshot.value}")
             }
         } catch (e: Exception) {
-            Log.e("SensorDataRepository", "Error fetching sensor data: ${e.message}")
-            null
+            Log.e(tag, "Failed to fetch sensor data", e)
         }
     }
 }
