64e481510176e496540ec7e49c67a6a01e047cc5
cbrf2002 <cbrf2002@proton.me>
2024-10-04 18:19:14 +0800

    Added some pagination to prevent reaching firestore quota so fast.

--- app/src/main/java/com/fsvdevs/agrosphere/viewmodel/SensorDataViewModel.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/viewmodel/SensorDataViewModel.kt
@@ -1,37 +1,49 @@
 package com.fsvdevs.agrosphere.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.fsvdevs.agrosphere.models.SensorData
 import com.fsvdevs.agrosphere.repository.SensorDataRepository
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
 class SensorDataViewModel(private val repository: SensorDataRepository) : ViewModel() {
-    private val tag = "SensorDataViewModel"
-
     val sensorData: StateFlow<SensorData?> = repository.sensorData
 
-    init {
-        startFetchingSensorData()
-    }
+    private val _selectedTimeRange = MutableStateFlow("Hour")
+    val selectedTimeRange: StateFlow<String> = _selectedTimeRange
 
-    private fun startFetchingSensorData() {
+    private val _sensorHistory = MutableStateFlow<List<SensorData>>(emptyList())
+    val sensorHistory: StateFlow<List<SensorData>> = _sensorHistory
+
+    // Caching sensor history to avoid multiple Firestore reads for the same time range
+    private val cachedSensorHistory = mutableMapOf<String, List<SensorData>>()
+
+    // Debouncing to avoid redundant fetches
+    private var lastFetchTime: Long = 0
+    private val fetchInterval = 600000L // 10 minutes
+
+    fun fetchSensorHistoryByRange(timeRange: String) {
         viewModelScope.launch {
-            while (isActive) {
-                loadSensorData()
-                delay(5000)
+            val currentTime = System.currentTimeMillis()
+            if (cachedSensorHistory.containsKey(timeRange) && currentTime - lastFetchTime < fetchInterval) {
+                _sensorHistory.value = cachedSensorHistory[timeRange] ?: emptyList()
+            } else {
+                val data = repository.getSensorHistoryByRange(timeRange)
+                cachedSensorHistory[timeRange] = data
+                _sensorHistory.value = data
+                lastFetchTime = currentTime
             }
         }
     }
 
-    private fun loadSensorData() {
-        viewModelScope.launch {
-            Log.d(tag, "Loading sensor data")
-            repository.fetchSensorData()
-        }
+    init {
+        fetchSensorHistoryByRange("Hour")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        repository.stopListeningForSensorData()
     }
 }
