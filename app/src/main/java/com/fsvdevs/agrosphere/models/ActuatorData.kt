d9c32016999c298b94ac0ea83296d4cf9360bd95
cbrf2002 <cbrf2002@proton.me>
2024-10-23 16:03:02 +0800

    Added auto data for automated climate control mechanism

--- app/src/main/java/com/fsvdevs/agrosphere/models/ActuatorData.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/models/ActuatorData.kt
@@ -3,5 +3,6 @@ package com.fsvdevs.agrosphere.models
 data class ActuatorData(
     var fan: Boolean = false,
     var mist: Boolean = false,
-    var vent: Boolean = false
+    var vent: Boolean = false,
+    var auto: Boolean = false
 )
