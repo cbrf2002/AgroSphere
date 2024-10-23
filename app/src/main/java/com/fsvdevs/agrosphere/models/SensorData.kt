3ffacb961d32fdd4e832f476bed1d282713e58c3
cbrf2002 <cbrf2002@proton.me>
2024-10-23 16:06:04 +0800

    Added co2 and tds

--- app/src/main/java/com/fsvdevs/agrosphere/models/SensorData.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/models/SensorData.kt
@@ -1,11 +1,13 @@
 package com.fsvdevs.agrosphere.models
 
 data class SensorData(
+    var carbonDioxide: Double = 0.0,
     var humidity: Double = 0.0,
     var lightLevel: Double = 0.0,
     var pH: Double = 0.0,
     var temperature: Double = 0.0,
     var timestamp: Long = 0L,
     var waterLevel: Double = 0.0,
-    var waterTemp: Double = 0.0
+    var waterTemp: Double = 0.0,
+    var tds: Double = 0.0
 )
