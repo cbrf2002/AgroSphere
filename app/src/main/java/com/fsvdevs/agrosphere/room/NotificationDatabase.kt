5c25165b371ec9afe4cf21a00acef8fff34a6243
cbrf2002 <cbrf2002@proton.me>
2024-10-24 22:54:45 +0800

    Notification backend

--- app/src/main/java/com/fsvdevs/agrosphere/room/NotificationDatabase.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/room/NotificationDatabase.kt
@@ -1,4 +1,30 @@
 package com.fsvdevs.agrosphere.room
 
-class NotificationDatabase {
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import android.content.Context
+import com.fsvdevs.agrosphere.dao.NotificationDao
+import com.fsvdevs.agrosphere.models.NotificationData
+
+@Database(entities = [NotificationData::class], version = 1, exportSchema = false)
+abstract class NotificationDatabase : RoomDatabase() {
+    abstract fun notificationDao(): NotificationDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: NotificationDatabase? = null
+
+        fun getDatabase(context: Context): NotificationDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NotificationDatabase::class.java,
+                    "notification_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
