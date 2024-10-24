88fc50123eebabc39779d30027cfa647c4a05b23
cbrf2002 <cbrf2002@proton.me>
2024-10-25 01:03:53 +0800

    Consolidated theme

--- app/src/main/java/com/fsvdevs/agrosphere/ui/NotificationScreen.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/ui/NotificationScreen.kt
@@ -60,109 +60,107 @@ fun NotificationsScreen() {
         notifications.value = notificationRepository.getNotifications()
     }
 
-    AgroSphereTheme(isSystemInDarkTheme()) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
-                .systemBarsPadding()
-        ) {
-            // Confirmation dialog for deletion
-            if (showDeleteConfirmation.value) {
-                ConfirmationDialog(
-                    coroutineScope = coroutineScope,
-                    showDeleteConfirmation = showDeleteConfirmation,
-                    notificationRepository = notificationRepository,
-                    notifications = notifications
-                )
-            }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .systemBarsPadding()
+    ) {
+        // Confirmation dialog for deletion
+        if (showDeleteConfirmation.value) {
+            ConfirmationDialog(
+                coroutineScope = coroutineScope,
+                showDeleteConfirmation = showDeleteConfirmation,
+                notificationRepository = notificationRepository,
+                notifications = notifications
+            )
+        }
 
-            // Display notifications or a message if none available
-            if (notifications.value.isEmpty()) {
-                Text(
-                    text = "No notifications available",
-                    style = AppTypography.bodyMedium,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.fillMaxWidth()
-                )
-            } else {
-                Card(
-                    shape = RoundedCornerShape(24.dp),
-                    colors = CardDefaults.cardColors(
-                        containerColor = MaterialTheme.colorScheme.surfaceContainerLowest
-                    ),
-                    modifier = Modifier
-                        .fillMaxWidth()
+        // Display notifications or a message if none available
+        if (notifications.value.isEmpty()) {
+            Text(
+                text = "No notifications available",
+                style = AppTypography.bodyMedium,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        } else {
+            Card(
+                shape = RoundedCornerShape(24.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceContainerLowest
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
-                    Column(
-                        modifier = Modifier.padding(16.dp),
-                        verticalArrangement = Arrangement.spacedBy(8.dp)
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
                     ) {
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                            verticalAlignment = Alignment.CenterVertically,
+                        Text(
+                            text = "Historical Notifications",
+                            style = MaterialTheme.typography.titleLarge.copy(),
+                            fontWeight = FontWeight.Black,
+                            modifier = Modifier.wrapContentWidth(),
+                            textAlign = TextAlign.Left
+                        )
+                        Spacer(modifier = Modifier.padding(8.dp))
+                        OutlinedButton(
+                            onClick = { showDeleteConfirmation.value = true },
+                            modifier = Modifier.wrapContentWidth()
                         ) {
                             Text(
-                                text = "Historical Notifications",
-                                style = MaterialTheme.typography.titleLarge.copy(),
-                                fontWeight = FontWeight.Black,
-                                modifier = Modifier.wrapContentWidth(),
-                                textAlign = TextAlign.Left
+                                text = "Delete All Notifications",
+                                style = MaterialTheme.typography.labelSmall,
+                                textAlign = TextAlign.Center,
+                                color = MaterialTheme.colorScheme.primary
                             )
-                            Spacer(modifier = Modifier.padding(8.dp))
-                            OutlinedButton(
-                                onClick = { showDeleteConfirmation.value = true },
-                                modifier = Modifier.wrapContentWidth()
-                            ) {
-                                Text(
-                                    text = "Delete All Notifications",
-                                    style = MaterialTheme.typography.labelSmall,
-                                    textAlign = TextAlign.Center,
-                                    color = MaterialTheme.colorScheme.primary
-                                )
-                            }
                         }
-                        Spacer(modifier = Modifier.height(8.dp))
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    HorizontalDivider(
+                        color = MaterialTheme.colorScheme.outlineVariant,
+                        thickness = 1.dp
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    notifications.value.forEach { notification ->
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(
+                                text = notification.title,
+                                textAlign = TextAlign.Start,
+                                style = AppTypography.bodyLarge,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.fillMaxWidth().weight(.4f)
+                            )
+                            Spacer(modifier = Modifier.weight(.2f))
+                            Text(
+                                text = formatNotificationTimestamp(notification.timestamp),
+                                textAlign = TextAlign.End,
+                                style = AppTypography.bodySmall,
+                                modifier = Modifier.fillMaxWidth().weight(.4f)
+                            )
+                        }
+                        Text(
+                            text = notification.message,
+                            textAlign = TextAlign.Start,
+                            style = AppTypography.bodySmall,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
                         HorizontalDivider(
                             color = MaterialTheme.colorScheme.outlineVariant,
                             thickness = 1.dp
                         )
-                        Spacer(modifier = Modifier.height(8.dp))
-                        notifications.value.forEach { notification ->
-                            Row(
-                                horizontalArrangement = Arrangement.SpaceBetween,
-                                verticalAlignment = Alignment.CenterVertically,
-                                modifier = Modifier.fillMaxWidth()
-                            ) {
-                                Text(
-                                    text = notification.title,
-                                    textAlign = TextAlign.Start,
-                                    style = AppTypography.bodyLarge,
-                                    fontWeight = FontWeight.Bold,
-                                    modifier = Modifier.fillMaxWidth().weight(.4f)
-                                )
-                                Spacer(modifier = Modifier.weight(.2f))
-                                Text(
-                                    text = formatNotificationTimestamp(notification.timestamp),
-                                    textAlign = TextAlign.End,
-                                    style = AppTypography.bodySmall,
-                                    modifier = Modifier.fillMaxWidth().weight(.4f)
-                                )
-                            }
-                            Text(
-                                text = notification.message,
-                                textAlign = TextAlign.Start,
-                                style = AppTypography.bodySmall,
-                                modifier = Modifier.fillMaxWidth()
-                            )
-                            Spacer(modifier = Modifier.height(4.dp))
-                            HorizontalDivider(
-                                color = MaterialTheme.colorScheme.outlineVariant,
-                                thickness = 1.dp
-                            )
-                            Spacer(modifier = Modifier.height(4.dp))
-                        }
+                        Spacer(modifier = Modifier.height(4.dp))
                     }
                 }
             }
