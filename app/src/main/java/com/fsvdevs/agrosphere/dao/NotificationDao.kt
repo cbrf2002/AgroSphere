efbc0adde3812dd48a5390fe3acfab3b5e9a3cd8
cbrf2002 <cbrf2002@proton.me>
2024-10-24 22:54:24 +0800

    Notification backend

--- app/src/main/java/com/fsvdevs/agrosphere/dao/NotificationDao.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/dao/NotificationDao.kt
@@ -1,4 +1,18 @@
 package com.fsvdevs.agrosphere.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.fsvdevs.agrosphere.models.NotificationData
+
+@Dao
 interface NotificationDao {
+    @Insert
+    suspend fun insertNotification(notification: NotificationData)
+
+    @Query("SELECT * FROM notifications ORDER BY timestamp DESC")
+    suspend fun getAllNotifications(): List<NotificationData>
+
+    @Query("DELETE FROM notifications")
+    suspend fun clearAllNotifications()
 }
