87f380128964ddc68aeb6286c8fd256310838439
cbrf2002 <cbrf2002@proton.me>
2024-10-24 22:54:33 +0800

    Notification backend

--- app/src/main/java/com/fsvdevs/agrosphere/models/NotificationData.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/models/NotificationData.kt
@@ -1,3 +1,12 @@
 package com.fsvdevs.agrosphere.models
 
-data class NotificationData()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notifications")
+data class NotificationData(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,  // Room will automatically assign a unique ID
+    val title: String,
+    val message: String,
+    val timestamp: Long
+)
