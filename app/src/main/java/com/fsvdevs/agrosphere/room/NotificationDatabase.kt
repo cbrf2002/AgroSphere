5aef62f3552f339c06548e34010f18ece0724189
cbrf2002 <cbrf2002@proton.me>
2024-10-26 17:42:23 +0800

    Optimized imports

--- app/src/main/java/com/fsvdevs/agrosphere/room/NotificationDatabase.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/room/NotificationDatabase.kt
@@ -1,9 +1,9 @@
 package com.fsvdevs.agrosphere.room
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import android.content.Context
 import com.fsvdevs.agrosphere.dao.NotificationDao
 import com.fsvdevs.agrosphere.models.NotificationData
 
