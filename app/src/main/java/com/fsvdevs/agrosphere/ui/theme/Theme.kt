6592795a4a976017d4b89b8d8e537cb899b8f49b
cbrf2002 <cbrf2002@proton.me>
2024-10-26 17:45:01 +0800

    Optimized imports

--- app/src/main/java/com/fsvdevs/agrosphere/ui/theme/Theme.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/ui/theme/Theme.kt
@@ -2,9 +2,7 @@ package com.fsvdevs.agrosphere.ui.theme
 
 import android.app.Activity
 import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
