83ac818cd080e7325363d7604b7d7ede378a9188
cbrf2002 <cbrf2002@proton.me>
2024-10-27 21:33:39 +0800

    Optimized imports

--- app/src/main/java/com/fsvdevs/agrosphere/ui/LoginScreen.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/ui/LoginScreen.kt
@@ -3,7 +3,6 @@ package com.fsvdevs.agrosphere.ui
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
