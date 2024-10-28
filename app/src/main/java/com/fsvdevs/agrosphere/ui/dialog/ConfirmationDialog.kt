f40408ad737b1fc41f2752eabca1bf7eef01896a
cbrf2002 <cbrf2002@proton.me>
2024-10-28 20:52:44 +0800

    Corrected UI scaling based on the scaling options of the phone and its DPI.

--- app/src/main/java/com/fsvdevs/agrosphere/ui/dialog/ConfirmationDialog.kt
+++ app/src/main/java/com/fsvdevs/agrosphere/ui/dialog/ConfirmationDialog.kt
@@ -17,15 +17,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.fsvdevs.agrosphere.R
 import com.fsvdevs.agrosphere.models.NotificationData
 import com.fsvdevs.agrosphere.repository.NotificationRepository
+import com.fsvdevs.agrosphere.util.DensityHelper.getScaledDensity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -36,59 +39,76 @@ fun ConfirmationDialog(
     notificationRepository: NotificationRepository,
     notifications: MutableState<List<NotificationData>>
 ) {
-    Card(
-        modifier = Modifier.fillMaxWidth().padding(24.dp),
-        shape = RoundedCornerShape(24.dp),
-        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceContainerHigh)
-    ) {
-        Column(
-            modifier = Modifier.padding(24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+    CompositionLocalProvider(LocalDensity provides getScaledDensity()) {
+        Card(
+            modifier = Modifier.fillMaxWidth().padding(24.dp),
+            shape = RoundedCornerShape(24.dp),
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceContainerHigh)
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.rounded_notifications_off_24),
-                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
-                contentDescription = "Notifications off",
-                modifier = Modifier.size(32.dp)
+            ConfirmationDialogInterface(
+                coroutineScope = coroutineScope,
+                showDeleteConfirmation = showDeleteConfirmation,
+                notificationRepository = notificationRepository,
+                notifications = notifications
             )
+        }
+    }
+}
 
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = "Delete All Notifications",
-                style = MaterialTheme.typography.headlineSmall
-            )
+@Composable
+fun ConfirmationDialogInterface(
+    coroutineScope: CoroutineScope,
+    showDeleteConfirmation: MutableState<Boolean>,
+    notificationRepository: NotificationRepository,
+    notifications: MutableState<List<NotificationData>>
+) {
+    Column(
+        modifier = Modifier.padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.rounded_notifications_off_24),
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+            contentDescription = "Notifications off",
+            modifier = Modifier.size(32.dp)
+        )
 
-            Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Delete All Notifications",
+            style = MaterialTheme.typography.headlineSmall
+        )
 
-            Text(
-                text = "Are you sure you want to delete all notifications?",
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.onSurfaceVariant
-            )
-            Spacer(modifier = Modifier.height(24.dp))
+        Spacer(modifier = Modifier.height(16.dp))
 
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.End,
-                verticalAlignment = Alignment.CenterVertically
+        Text(
+            text = "Are you sure you want to delete all notifications?",
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        notificationRepository.clearNotifications()
+                        notifications.value = emptyList()
+                    }
+                    showDeleteConfirmation.value = false
+                },
+                modifier = Modifier.padding(end = 8.dp)
             ) {
-                Button(
-                    onClick = {
-                        coroutineScope.launch {
-                            notificationRepository.clearNotifications()
-                            notifications.value = emptyList()
-                        }
-                        showDeleteConfirmation.value = false
-                    },
-                    modifier = Modifier.padding(end = 8.dp)
-                ) {
-                    Text("Yes")
-                }
-                OutlinedButton(
-                    onClick = { showDeleteConfirmation.value = false }
-                ) {
-                    Text("No")
-                }
+                Text("Yes")
+            }
+            OutlinedButton(
+                onClick = { showDeleteConfirmation.value = false }
+            ) {
+                Text("No")
             }
         }
     }
