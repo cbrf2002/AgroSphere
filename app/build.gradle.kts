d03193c97e2ab264102ab39c3de51c7d61b94d10
cbrf2002 <cbrf2002@proton.me>
2025-02-09 17:13:47 +0800

    Update v1.37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         minSdk = 29
         //noinspection EditedTargetSdkVersion
         targetSdk = 35
-        versionCode = 240
-        versionName = "1.36"
+        versionCode = 245
+        versionName = "1.37"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
